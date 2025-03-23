7d7679af58b5132d8682b9952600279304b9bb52
Wing.C4D <mikmuk1@gmail.com>
2025-03-24 00:57:22 +0200

    Initial TCP/IP header dissector with dynamic offset handling

---
+++ packet_dissector.py
@@ -0,0 +1,35 @@
+import socket
+import struct
+class PacketDissector():
+    
+    def calculate_offset(packet: bytes, has_ethernet = True):
+    #Ethernet Logic
+
+        offset = 0
+        if has_ethernet:
+            eth_header_len = 14
+            offset += eth_header_len
+        else:
+            eth_header_len = 0
+
+    #IP Header Logic
+        ip_header_start = offset
+        ip_header = packet[ip_header_start : ip_header_start + 20]
+        iphl = packet[ip_header_start] & 0x0F
+        ip_header_length = iphl * 4
+        offset += ip_header_length
+
+    #Tcp Header Logic
+        tcp_header_start = offset
+        tcp_header = packet[tcp_header_start: tcp_header_start + 20]
+        tcp_data_offset = (packet[tcp_header_start + 12] >> 4) * 4
+        tcp_header_length = tcp_data_offset
+        offset += tcp_header_length
+
+        return {
+            "eth_header_len" : eth_header_len,
+            "ip_header_len" : ip_header_length,
+            "tcp_header_len" : tcp_header_length,
+            "payload_start" : offset,
+            "packet_size" : len(packet)
+        }
